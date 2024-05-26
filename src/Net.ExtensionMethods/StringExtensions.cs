eaa5b2b7f9a253d1906f32c813d912ee99397ab3
Adewole <wolecharles@gmail.com>
2024-05-26 16:01:32 +0100

    added string extensions

---
+++ src/Net.ExtensionMethods/StringExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Net.ExtensionMethods
+{
+    public static class StringExtensions
+    {
+        public static bool IsNullOrEmpty(this string source)
+        {
+            return source == null  || !source.Any();
+        }
+    }
+}
---
+++ tests/Net.ExtensionMethodsTests/StringExtensionsTests.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Net.ExtensionMethods;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Net.ExtensionMethods.Tests
+{
+    [TestClass()]
+    public class StringExtensionsTests
+    {
+        [TestMethod()]
+        public void Should_Be_True_When_String_Is_Null()
+        {
+            //Arrange
+            string word = null;
+
+            //Act
+            var isNull = word.IsNullOrEmpty();
+
+            //Assert
+           Assert.AreEqual(true, isNull);
+        }
+
+        [TestMethod()]
+        public void Should_Be_True_When_String_Is_Empty()
+        {
+            //Arrange
+            string word = string.Empty;
+
+            //Act
+            var isNull = word.IsNullOrEmpty();
+
+            //Assert
+            Assert.AreEqual(true, isNull);
+        }
+
+        [TestMethod()]
+        public void Should_Be_False_When_String_Is_Not_Empty()
+        {
+            //Arrange
+            string word = "Hey there";
+
+            //Act
+            var isNull = word.IsNullOrEmpty();
+
+            //Assert
+            Assert.AreEqual(false, isNull);
+        }
+
+    }
+}
