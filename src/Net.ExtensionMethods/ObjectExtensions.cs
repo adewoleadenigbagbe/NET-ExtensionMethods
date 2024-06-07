c9f291b66ab31997ac922bcca08a411c1e5d2798
Adewole <wolecharles@gmail.com>
2024-06-07 23:06:48 +0100

    added object extensions

---
+++ src/Net.ExtensionMethods/ObjectExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Net.ExtensionMethods
+{
+    public static class ObjectExtensions
+    {
+        public static TTarget ConvertToType<TTarget>(this object source) where TTarget : class
+        {
+            var target = source as TTarget;
+
+            return target;
+        }
+
+
+        public static TTarget ConvertToAnyType<TTarget>(this object source) 
+        {
+            var target = (TTarget)source ;
+
+            return target;
+        }
+    }
+}
---
+++ tests/Net.ExtensionMethodsTests/ObjectExtensionsTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Net.ExtensionMethods;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Net.ExtensionMethods.Tests
+{
+    [TestClass()]
+    public class ObjectExtensionsTests
+    {
+        [TestMethod()]
+        public void Should_Convert_Object_To_String()
+        {
+            //Arrange
+            object obj1 = "A book";
+
+            //Act
+            var str1 = obj1.ConvertToType<string>();
+
+            //Assert
+            Assert.AreEqual(str1.GetType(), typeof(string));
+
+        }
+
+        [TestMethod()]
+        public void Should_Convert_Object_To_Int()
+        {
+            //Arrange
+            object obj1 = 34;
+
+            //Act
+            var num = obj1.ConvertToAnyType<int>();
+
+            //Assert
+            Assert.AreEqual(num.GetType(), typeof(int));
+
+        }
+    }
+}
